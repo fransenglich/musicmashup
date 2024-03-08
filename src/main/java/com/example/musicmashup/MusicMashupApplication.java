77537677d01b32c43b02af144873bf3ae136ea03
Frans Englich <fenglich@fastmail.fm>
2024-03-08 11:11:12 +0100

    From new template.

---
+++ src/main/java/com/example/musicmashup/MusicMashupApplication.java
@@ -0,0 +1,13 @@
+package com.example.musicmashup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicMashupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicMashupApplication.class, args);
+	}
+
+}
