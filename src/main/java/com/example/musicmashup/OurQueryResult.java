e98841cc372752202b3284e20d29b2a39bd2b9a4
Frans Englich <fenglich@fastmail.fm>
2024-03-12 10:30:27 +0100

    Comment

--- src/main/java/com/example/musicmashup/OurQueryResult.java
+++ src/main/java/com/example/musicmashup/OurQueryResult.java
@@ -2,4 +2,11 @@ package com.example.musicmashup;
 
 import java.util.List;
 
+/**
+ * A Java record for our JSON result.
+ * It is the MBID followed by the list of albums.
+ *
+ * @param mbid The MBID of the artist
+ * @param albums A list of the artist's albums.
+ */
 public record OurQueryResult(String mbid, List<OurResultAlbum> albums) { }
